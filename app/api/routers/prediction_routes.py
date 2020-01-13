246350c6265d744eee526e234b7e1c20d6709a0b
David.Schemm <david.schemm@tum.de>
2020-01-13 14:05:56 +0100

    fix emission_output deletion

--- app/api/routers/prediction_routes.py
+++ app/api/routers/prediction_routes.py
@@ -18,20 +18,20 @@ async def start_prediction(inputs: PredictionInput = example_prediction_input, d
     """
     Training and prediction using a Long-Short-Term-Memory Recurrent Neural Network
     """
-    try:
-        # df_traffic = await get_latest_bremicker(db, inputs.start_hour, inputs.end_hour)
-        df_traffic = await get_bremicker_by_time(db, start_hour=inputs.start_hour, end_hour=inputs.end_hour)
-        if inputs.vehicleNumber is None:
-            inputs.vehicleNumber = round(df_traffic.sum(axis=1, skipna=True).sum(axis=0) * 1.25)
-            print("Simulation with %s vehicles" % str(inputs.vehicleNumber))
-        sim_id = generate_id(inputs)
-        # print('sim id:', sim_id)
-        result = await Predictor(db, inputs, sim_id, df_traffic=df_traffic, is_single_sim=False).predict_emissions()
-        await delete_simulation_files()
-        return result
-    except Exception as e:
-        print(str(e))
-        raise HTTPException(status_code=500, detail=str(e))
+    # try:
+    # df_traffic = await get_latest_bremicker(db, inputs.start_hour, inputs.end_hour)
+    df_traffic = await get_bremicker_by_time(db, start_hour=inputs.start_hour, end_hour=inputs.end_hour)
+    if inputs.vehicleNumber is None:
+        inputs.vehicleNumber = round(df_traffic.sum(axis=1, skipna=True).sum(axis=0) * 1.25)
+        print("Simulation with %s vehicles" % str(inputs.vehicleNumber))
+    sim_id = generate_id(inputs)
+    # print('sim id:', sim_id)
+    result = await Predictor(db, inputs, sim_id, df_traffic=df_traffic, is_single_sim=False).predict_emissions()
+    await delete_simulation_files()
+    return result
+    # except Exception as e:
+    #     print(str(e))
+    #     raise HTTPException(status_code=500, detail=str(e))
 
 @router.post('/prediction/single')
 async def start_single_prediction(inputs: PredictionInput = example_prediction_input, db: AsyncIOMotorClient=Depends(get_database)):
--- app/api/routers/utils.py
+++ app/api/routers/utils.py
@@ -16,11 +16,12 @@ def generate_single_id(inputs):
 async def delete_simulation_files():
     folder = EMISSION_OUTPUT_BASE
     for filename in os.listdir(folder):
-        file_path = os.path.join(folder, filename)
-        try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-        except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
+        if ".py" not in filename:
+            file_path = os.path.join(folder, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print('Failed to delete %s. Reason: %s' % (file_path, e))
